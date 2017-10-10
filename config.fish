dbf6e92bcdd9a148bfeef60c037daba902ca07a9
Seunghun Lee <waydi1@gmail.com>
2017-10-10 16:52:05 +0900

    Initial commit

---
+++ config.fish
@@ -0,0 +1,8 @@
+# ls alias
+alias l='ls -alh'
+alias ll= 'ls -lh'
+
+# vim alias
+alias vim Vim
+alias vi='nvim'
+alias vim='nvim'
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.config/fish && ln -sf ~/.dotfiles/config.fish ~/.config/fish/config.fish
