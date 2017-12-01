ad39c4e7b0ab584407c8912ee6b5f254f48492fa
Seunghun Lee <waydi1@gmail.com>
2017-12-01 19:55:06 +0900

    Rename .zshrc to .zshrc.local if it exists

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-
+if [ -f ~/.zshrc ]; then
+    mv ~/.zshrc ~/.zshrc.local
+fi
 ln -sf .dotfiles/.zshrc ~/.zshrc
 ln -sf .dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf .dotfiles/.ideavimrc ~/.ideavimrc 
